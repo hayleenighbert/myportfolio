96861b281a71c20b8054836be1c7c6ec7750bdce
Haylee Nighbert <haylee.nigh@gmail.com>
2019-05-19 16:02:45 -0700

    Zoom animation for portfolio section

--- js/index.js
+++ js/index.js
@@ -1,15 +1,6 @@
-var app = document.getElementById('typewriter');
-
-var typewriter = new Typewriter(app, {
-    loop: true
+$(".pic").hover(
+  function(){
+  		$(".info", this).css("display", "block");
+	}, function(){
+  		$(".info", this).css("display", "none");
 });
-
-typewriter.typeString('UX Designer')
-    .pauseFor(1000)
-    .deleteAll()
-    .typeString('Web Developer')
-    .pauseFor(1000)
-    .deleteChars(7)
-    .typeString('Interactive Designer')
-    .pauseFor(1000)
-    .start();
